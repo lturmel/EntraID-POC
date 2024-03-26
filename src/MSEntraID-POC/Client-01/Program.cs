e488ca53a604d69a25394b623c548ced741971c3
Louis Turmel <lturmel@expertasolutions.com>
2024-03-26 13:45:23 -0400

    update

--- src/MSEntraID-POC/Client-01/Program.cs
+++ src/MSEntraID-POC/Client-01/Program.cs
@@ -13,6 +13,14 @@ var adClientId = "d76c2277-f2c0-45ed-a242-b0ae5da25b77";
 var secretId = "addd86b7-0d75-4de7-a8d5-6c1175f5b241";
 var value = "C3U8Q~qJxaD2vtHXn0zUG90W6~Dr~dtHet8m1a61";
 
+/*
+   "Instance": "https://login.microsoftonline.com/",
+   "Domain": "expertasolutions.com",
+   "TenantId": "b046897f-c12f-49a4-9d20-65b78e454376",
+   "ClientId": "907e3ef0-2849-4f68-8e26-e4d6bc204713",
+   "Scopes": "access_as_user"
+*/
+
 // TODO: Get Jwt Token from Azure AD
 var authUrl = $"https://login.microsoftonline.com/{adTenantId}/oauth2/token";
 var tokenClient = new HttpClient();
@@ -26,6 +34,7 @@ var authContent = new List<KeyValuePair<string, string>> {
     { new KeyValuePair<string, string>("grant_type", "client_credentials") },
     { new KeyValuePair<string, string>("client_id", adClientId) },
     { new KeyValuePair<string, string>("client_secret", value) },
+    { new KeyValuePair<string, string>("scope", "api://907e3ef0-2849-4f68-8e26-e4d6bc204713/access_as_user")},
     { new KeyValuePair<string, string>("resource", "api://907e3ef0-2849-4f68-8e26-e4d6bc204713") }
 };
 
@@ -33,17 +42,18 @@ var result = await tokenClient.PostAsync(authUrl, new FormUrlEncodedContent(auth
 
 var token = JsonConvert.DeserializeObject<dynamic>(await result.Content.ReadAsStringAsync());
 Console.WriteLine(token);
-var tokenString = token.access_token;
-Console.WriteLine(tokenString);
 
-// Call API with Bearer Token that came from MS Entra ID
-
-var apiUrl = "https://localhost:7212/weatherforecast/";
-
-// //Console.WriteLine("Calling API: " + apiUrl);
+// var tokenString = token.access_token;
+// Console.WriteLine(tokenString);
+//
+// // Call API with Bearer Token that came from MS Entra ID
+//
+// var apiUrl = "https://localhost:7212/weatherforecast/";
+//
+// Console.WriteLine("Calling API: " + apiUrl);
 //
 // var client = new HttpClient();
-// client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+// //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 // client.DefaultRequestHeaders.Add("Authorization", "Bearer " + tokenString);
 //
 // var response = await client.GetAsync(apiUrl);
--- src/MSEntraID-POC/WebService-A/Program.cs
+++ src/MSEntraID-POC/WebService-A/Program.cs
@@ -1,7 +1,5 @@
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
-using Microsoft.Identity.Abstractions;
 using Microsoft.Identity.Web.Resource;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,13 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-builder.Services.AddAuthorization();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
